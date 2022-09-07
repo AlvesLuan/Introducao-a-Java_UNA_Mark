ebe3c8376953f34a703b92cae282ee8e8962597b
Luan K. Alves <luankauan10@gmail.com>
2022-09-07 13:20:12 -0300

    Create SomarBoleto.java

---
+++ Avulso/SomarBoleto.java
@@ -0,0 +1,35 @@
+package aulasuna;
+import java.util.*;
+public class SomarBoleto {
+
+    public static void main(String[] args)
+    {  
+        float salarioLiquido, boleto1, boleto2, boleto3, boleto4, totalDespesas, sobras;
+
+        Scanner Entrada;
+        Entrada = new Scanner(System.in);
+
+        System.out.println("Informe seu Salário Liquido: ");
+        salarioLiquido = Entrada.nextFloat();
+
+        System.out.println("Informe o valor do primeiro boleto: ");
+        boleto1 = Entrada.nextFloat();
+        System.out.println("Informe o valor segundo boleto: ");
+        boleto2 = Entrada.nextFloat();
+        System.out.println("Informe o valor terceiro boleto: ");
+        boleto3 = Entrada.nextFloat();
+        System.out.println("Informe o valor do quarto boleto: ");
+        boleto4 = Entrada.nextFloat();
+
+        totalDespesas = (boleto1 + boleto2 + boleto3 + boleto4);
+        System.out.println("\nSuas despesas totais são R$"+totalDespesas);
+
+        if (totalDespesas > salarioLiquido){
+            sobras = totalDespesas - salarioLiquido;
+            System.out.println("Vai faltar R$"+sobras+" ai parceiro");} 
+
+        else{
+            sobras = salarioLiquido - totalDespesas;
+            System.out.println("Sobrará R$"+sobras+" pra vc brincar!!");}      
+    }    
+}
